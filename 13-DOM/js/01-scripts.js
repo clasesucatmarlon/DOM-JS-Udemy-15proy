50cbc7f1bfdfe85c5d89e9aa1c6bd353041da0df
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-11-30 12:56:09 -0500

    Acceder a elementos del HTML con document

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,21 @@
+let elemento;
+
+elemento = document;
+elemento = document;
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+
+// Formularios
+elemento = document.forms;
+elemento = document.forms[0].className;
+elemento = document.forms[0].method;
+elemento = document.forms[0].classList;
+elemento = document.forms[0].action;
+
+// elnaces
+elemento = document.links;
+elemento = document.links[4];
+
+
+console.log(elemento);
