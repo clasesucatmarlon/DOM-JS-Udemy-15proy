0a3480a723aba95b7d165261e3fe2bd7b87debf7
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-12-01 19:02:53 -0500

    Cambiar CSS con JS

---
+++ 13-DOM/js/07-scripts.js
@@ -0,0 +1,11 @@
+// Cambiar css con js
+
+const encabezado = document.querySelector('h1');
+
+encabezado.style.color = 'blue';
+encabezado.style.fontSize = '50px';
+encabezado.style.textAlign = 'center';
+encabezado.style.textTransform = 'upperCase';
+
+
+console.log(encabezado.style);
